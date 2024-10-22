37e85c49daf0e88df3644e5a91c8126ca101d411
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2024-10-22 08:59:03 -0700

    pkgs/sagemath-groups: Move src/sage/geometry/palp_normal_form.pyx here from sagemath-polyhedra; fixes #52

--- src/sage/geometry/palp_normal_form.pyx
+++ src/sage/geometry/palp_normal_form.pyx
@@ -1,5 +1,5 @@
-# sage_setup: distribution = sagemath-polyhedra
-# sage.doctest: needs sage.groups
+# sage_setup: distribution = sagemath-groups
+# sage.doctest: needs sage.geometry.polyhedron sage.groups
 r"""
 PALP normal form of vertices of a lattice polytope
 """
