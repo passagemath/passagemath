3ca0e0fdce3d0c527b6c5dc6e2cdb563b59f7454
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2024-10-26 21:37:12 -0700

    pkgs/sagemath-gap/repair_wheel.py: Flush output

--- pkgs/sagemath-gap/repair_wheel.py
+++ pkgs/sagemath-gap/repair_wheel.py
@@ -12,8 +12,10 @@ wheel = sys.argv[1]
 
 for dir in GAP_ROOT_PATHS.split(';'):
     print(f'Adding {dir}')
+    sys.stdout.flush()
     parent = Path(dir).parent
     name = Path(dir).name
     command = f'cd {shlex.quote(str(parent))} && zip -r {shlex.quote(wheel)} {name}'
     print(f'Running {command}')
+    sys.stdout.flush()
     os.system(command)
