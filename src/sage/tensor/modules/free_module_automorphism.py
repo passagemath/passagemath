e3b9bd87687c8deffbaec8636a1c9e39854f203f
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2024-11-02 21:25:59 -0700

    src/sage/tensor/modules: Add # needs

--- src/sage/tensor/modules/free_module_automorphism.py
+++ src/sage/tensor/modules/free_module_automorphism.py
@@ -1075,6 +1075,7 @@ class FreeModuleAutomorphism(FreeModuleTensor, MultiplicativeGroupElement):
 
         EXAMPLES::
 
+            sage: # needs sage.libs.pari
             sage: M = FiniteRankFreeModule(QQ, 2, name='M')
             sage: e = M.basis('e')
             sage: a = M.automorphism([[1,1],[0,2]], name='a')
