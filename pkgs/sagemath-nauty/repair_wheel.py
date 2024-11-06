c8d68abfa1f5d47aa1eb5afc8e1f2d9bf28967e4
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2024-11-05 22:17:59 -0800

    pkgs/sagemath-{cddlib,nauty,palp}/repair_wheel.py: Quote braces in f-strings

--- pkgs/sagemath-nauty/repair_wheel.py
+++ pkgs/sagemath-nauty/repair_wheel.py
@@ -12,7 +12,7 @@ wheel = sys.argv[1]
 
 # SAGE_LOCAL/bin/* --> sage_wheels/bin/*
 # list of files from build/pkgs/nauty/spkg-install.in
-command = f'ln -sf {shlex.quote(SAGE_LOCAL)} sage_wheels && zip -r {shlex.quote(wheel)} sage_wheels/bin/{addedgeg,addptg,amtog,ancestorg,assembleg,biplabg,catg,complg,converseg,copyg,countg,countneg,cubhamg,deledgeg,delptg,dimacs2g,directg,dreadnaut,dretodot,dretog,edgetransg,genbg,genbgL,geng,gengL,genposetg,genquarticg,genrang,genspecialg,gentourng,gentreeg,genktreeg,hamheuristic,labelg,linegraphg,listg,multig,nbrhoodg,newedgeg,pickg,planarg,productg,ranlabg,ransubg,shortg,showg,subdivideg,twohamg,underlyingg,uniqg,vcolg,watercluster2,NRswitchg}'
+command = f'ln -sf {shlex.quote(SAGE_LOCAL)} sage_wheels && zip -r {shlex.quote(wheel)} sage_wheels/bin/{{addedgeg,addptg,amtog,ancestorg,assembleg,biplabg,catg,complg,converseg,copyg,countg,countneg,cubhamg,deledgeg,delptg,dimacs2g,directg,dreadnaut,dretodot,dretog,edgetransg,genbg,genbgL,geng,gengL,genposetg,genquarticg,genrang,genspecialg,gentourng,gentreeg,genktreeg,hamheuristic,labelg,linegraphg,listg,multig,nbrhoodg,newedgeg,pickg,planarg,productg,ranlabg,ransubg,shortg,showg,subdivideg,twohamg,underlyingg,uniqg,vcolg,watercluster2,NRswitchg}}'
 print(f'Running {command}')
 sys.stdout.flush()
 os.system(command)
