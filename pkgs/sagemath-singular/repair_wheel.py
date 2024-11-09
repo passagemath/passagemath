ecce820bb175117771cba759254891c879d7f4e0
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2024-11-09 15:39:22 -0800

    pkgs/sagemath-singular/repair_wheel.py: Fix up

--- pkgs/sagemath-singular/repair_wheel.py
+++ pkgs/sagemath-singular/repair_wheel.py
@@ -11,7 +11,7 @@ from sage_conf import SAGE_LOCAL
 wheel = sys.argv[1]
 
 # SAGE_LOCAL/bin/Singular --> sage_wheels/bin/Singular etc.
-command = f'ln -sf {shlex.quote(SAGE_LOCAL)} sage_wheels && zip -r {shlex.quote(wheel)} sage_wheels/bin/*Singular sage_wheels/{{libexec,share}}/singular sage_wheels/sahre/factory'
+command = f'ln -sf {shlex.quote(SAGE_LOCAL)} sage_wheels && zip -r {shlex.quote(wheel)} sage_wheels/bin/*Singular sage_wheels/{{libexec,share}}/singular sage_wheels/share/factory'
 print(f'Running {command}')
 sys.stdout.flush()
 os.system(command)
