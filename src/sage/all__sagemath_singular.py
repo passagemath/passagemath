1eed845761209df184e6a30661509aa1e6407d0b
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2024-11-09 14:21:19 -0800

    pkgs/sagemath-singular/tox.ini, src/sage/all__sagemath_singular.py: Set up for testing

--- src/sage/all__sagemath_singular.py
+++ src/sage/all__sagemath_singular.py
@@ -1,5 +1,9 @@
 # sage_setup: distribution = sagemath-singular
 
+from sage.all__sagemath_modules import *
+from sage.all__sagemath_flint import *
+from sage.all__sagemath_linbox import *
+
 from .algebras.all__sagemath_singular import *
 from .libs.all__sagemath_singular import *
 from .matrix.all__sagemath_singular import *
