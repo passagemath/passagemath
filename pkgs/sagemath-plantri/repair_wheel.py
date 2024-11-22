661c8124556099d15293b6e6efa6e3cd72146d33
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2024-11-21 23:39:56 -0800

    pkgs/sagemath-plantri/repair_wheel.py: install fullgen too

--- pkgs/sagemath-plantri/repair_wheel.py
+++ pkgs/sagemath-plantri/repair_wheel.py
@@ -11,7 +11,7 @@ from sage_conf import SAGE_LOCAL
 wheel = sys.argv[1]
 
 # SAGE_LOCAL/bin/plantri --> sage_wheels/bin/plantri
-command = f'ln -sf {shlex.quote(SAGE_LOCAL)} sage_wheels && zip -r {shlex.quote(wheel)} sage_wheels/bin/plantri'
+command = f'ln -sf {shlex.quote(SAGE_LOCAL)} sage_wheels && zip -r {shlex.quote(wheel)} sage_wheels/bin/{{plantri,fullgen}}'
 print(f'Running {command}')
 sys.stdout.flush()
 os.system(command)
