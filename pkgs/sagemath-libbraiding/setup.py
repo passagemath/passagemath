a1b7b0fff219c882284f3f708aa5f06c983fbbd7
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2024-12-04 16:28:50 -0800

    pkgs/sagemath-libbraiding/setup.py: Ship the pyx file as package_data

--- pkgs/sagemath-libbraiding/setup.py
+++ pkgs/sagemath-libbraiding/setup.py
@@ -8,4 +8,5 @@ sys.path.insert(0, os.path.dirname(__file__))
 from sage_setup import sage_setup
 
 sage_setup(['sagemath-libbraiding'],
-           spkgs=['libbraiding'])
+           spkgs=['libbraiding'],
+           package_data={})
