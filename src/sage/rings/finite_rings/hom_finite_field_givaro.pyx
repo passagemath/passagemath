f3f7c6b9820e30fe87f6b9ed4ba2030d5447c369
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2024-12-10 13:37:50 -0800

    sage.matrix.matrix_{integer,rational}_dense, sage.rings.finite_rings.hom_finite_field_givaro: Remove ntl linkage

--- src/sage/rings/finite_rings/hom_finite_field_givaro.pyx
+++ src/sage/rings/finite_rings/hom_finite_field_givaro.pyx
@@ -1,9 +1,5 @@
 # sage_setup: distribution = sagemath-linbox
-# distutils: libraries = givaro NTL_LIBRARIES gmp m
-# distutils: extra_compile_args = NTL_CFLAGS
-# distutils: include_dirs = NTL_INCDIR
-# distutils: library_dirs = NTL_LIBDIR
-# distutils: extra_link_args = NTL_LIBEXTRA
+# distutils: libraries = givaro gmp m
 # distutils: language = c++
 """
 Givaro finite field morphisms
