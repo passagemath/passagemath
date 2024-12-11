29dab4d3b21e07f647b46077389bdfe160a6b4b5
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2024-12-10 23:04:58 -0800

    pkgs/sagemath-ecl: Ship the maxima executable here too

--- pkgs/sagemath-ecl/repair_wheel.py
+++ pkgs/sagemath-ecl/repair_wheel.py
@@ -11,7 +11,8 @@ from sage_conf import SAGE_LOCAL
 wheel = sys.argv[1]
 
 # SAGE_LOCAL/bin/ecl --> sage_wheels/bin/ecl
-command = f'ln -sf {shlex.quote(SAGE_LOCAL)} sage_wheels && zip -r {shlex.quote(wheel)} sage_wheels/bin/ecl sage_wheels/lib/ecl-*'
+# Include lib/maxima/5.47.0/binary-ecl/maxima, which is linked through to libecl
+command = f'ln -sf {shlex.quote(SAGE_LOCAL)} sage_wheels && zip -r {shlex.quote(wheel)} sage_wheels/bin/ecl sage_wheels/lib/ecl-* sage_wheels/lib/maxima'
 print(f'Running {command}')
 sys.stdout.flush()
 os.system(command)
