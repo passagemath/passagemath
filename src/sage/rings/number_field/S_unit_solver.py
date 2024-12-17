2729ed56e0a80ebad5ffbcdd07984157b16c8400
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2024-12-16 18:13:37 -0800

    src/sage/rings/number_field/S_unit_solver.py: Add # needs

--- src/sage/rings/number_field/S_unit_solver.py
+++ src/sage/rings/number_field/S_unit_solver.py
@@ -1,5 +1,5 @@
 # sage_setup: distribution = sagemath-flint
-# sage.doctest: needs sage.rings.number_field sage.rings.padics
+# sage.doctest: needs sage.rings.number_field sage.rings.padics sage.symbolic
 r"""
 Solver for the `S`-unit equation `x + y = 1`
 
