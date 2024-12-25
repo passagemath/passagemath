60b2829cd6f058985615cddcd51d8f118fa71d68
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2024-12-24 18:55:35 -0800

    src/sage/stats/distributions/discrete_gaussian_lattice.py: Add # needs

--- src/sage/stats/distributions/discrete_gaussian_lattice.py
+++ src/sage/stats/distributions/discrete_gaussian_lattice.py
@@ -482,14 +482,14 @@ class DiscreteGaussianDistributionLatticeSampler(SageObject):
             sage: c = vector(ZZ, [7, 2, 5])
             sage: D = distributions.DiscreteGaussianDistributionLatticeSampler(ZZ^n, Sigma, c)
             sage: f = D.f
-            sage: nf = D._normalisation_factor_zz(); nf # This has not been properly implemented
+            sage: nf = D._normalisation_factor_zz(); nf # This has not been properly implemented    # needs fpylll
             78.6804...
 
         We can compute the expected number of samples before sampling a vector::
 
             sage: v = vector(ZZ, n, (11, 4, 8))
             sage: v.set_immutable()
-            sage: 1 / (f(v) / nf)
+            sage: 1 / (f(v) / nf)                                                                   # needs fpylll
             2553.9461...
 
             sage: counter = defaultdict(Integer); m = 0
@@ -497,7 +497,7 @@ class DiscreteGaussianDistributionLatticeSampler(SageObject):
             ....:     add_samples(1000)
             sage: sum(counter.values())  # random
             3000
-            sage: while abs(m*f(v)*1.0/nf/counter[v] - 1.0) >= 0.1:                     # needs sage.symbolic
+            sage: while abs(m*f(v)*1.0/nf/counter[v] - 1.0) >= 0.1:                     # needs fpylll sage.symbolic
             ....:     add_samples(1000)
 
         If the covariance provided is not positive definite, an error is thrown::
