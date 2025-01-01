0e42e3eb30d2605ea47d74ba80452cebcaafdab0
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2024-12-31 18:42:11 -0800

    pkgs/sagemath-combinat: Add sage.dynamics.finite_dynamical_system*

---
+++ src/sage/dynamics/all__sagemath_combinat.py
@@ -0,0 +1,12 @@
+# sage_setup: distribution = sagemath-combinat
+
+from sage.misc.lazy_import import lazy_import
+
+# Discrete dynamical systems
+lazy_import('sage.dynamics.finite_dynamical_system',
+            ['DiscreteDynamicalSystem'])
+
+lazy_import('sage.dynamics', 'finite_dynamical_system_catalog',
+            'finite_dynamical_systems')
+
+del lazy_import
