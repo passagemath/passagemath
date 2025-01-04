f25e4f78830ec235a3185ffae11782efb3ca8094
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2025-01-03 23:25:58 -0800

    src/sage/rings/function_field/all__sagemath_modules.py: Move drinfeld import here from all.py

--- src/sage/rings/function_field/all.py
+++ src/sage/rings/function_field/all.py
@@ -5,8 +5,3 @@ try:
     from sage.rings.function_field.all__sagemath_symbolics import *
 except ImportError:
     pass
-
-from sage.misc.lazy_import import lazy_import
-
-lazy_import("sage.rings.function_field.drinfeld_modules.drinfeld_module", "DrinfeldModule")
-del lazy_import
