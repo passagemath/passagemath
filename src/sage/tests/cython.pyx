47137a18361307782e19bfe6c3a0ca1870847de5
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2025-01-11 15:21:57 -0800

    pkgs/sagemath-objects: Add pyx files from src/sage/tests

--- src/sage/tests/cython.pyx
+++ src/sage/tests/cython.pyx
@@ -1,3 +1,4 @@
+# sage_setup: distribution = sagemath-objects
 """
 This file collects tests requiring Cython.
 """
--- src/sage/tests/stl_vector.pyx
+++ src/sage/tests/stl_vector.pyx
@@ -1,3 +1,4 @@
+# sage_setup: distribution = sagemath-objects
 # distutils: language = c++
 """
 Example of a class wrapping an STL vector
