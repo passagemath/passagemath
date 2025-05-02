05884d5e9f95d0b29e5a0125ec00e4d4e90ccf4b
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2025-05-01 21:02:20 -0700

    pkgs/sagemath-repl: Include Jupyter kernel in wheels

--- pkgs/sagemath-repl/setup.py
+++ pkgs/sagemath-repl/setup.py
@@ -1,3 +1,24 @@
-from setuptools import setup
+# PEP 517 builds do not have . in sys.path
+import os
+import sys
+sys.path.insert(0, os.path.dirname(__file__))
 
-setup()
+from sage_setup import sage_setup
+from sage_setup.command.sage_install import sage_develop, sage_install
+
+import sage.env
+import sage.ext_data
+sage.env.SAGE_EXTCODE = os.path.join(os.path.dirname(__file__),
+                                     'sage', 'ext_data')
+
+sage_setup(
+    ['sagemath-repl'],
+    cmdclass={
+        "develop":   sage_develop,
+        "install":   sage_install,
+    },
+    package_data={
+        "sage.doctest": ["tests/*"],
+        "sage.repl.rich_output": ["example*"],
+    }
+)
