0e9d87a24793baa2b0ac5c12401346939e3db736
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2025-06-09 14:52:07 -0700

    pkgs/*/repair_wheel.py: Use bash

--- pkgs/sagemath-cddlib/repair_wheel.py
+++ pkgs/sagemath-cddlib/repair_wheel.py
@@ -19,4 +19,4 @@ with InWheel(wheel, wheel):
     command = f'set -o pipefail; (cd {shlex.quote(SAGE_LOCAL)} && tar cf - --dereference bin/{{adjacency,adjacency_gmp,allfaces,allfaces_gmp,cddexec,cddexec_gmp,fourier,fourier_gmp,lcdd,lcdd_gmp,projection,projection_gmp,redcheck,redcheck_gmp,scdd,scdd_gmp,testcdd1,testcdd1_gmp,testcdd2,testcdd2_gmp,testlp1,testlp1_gmp,testlp2,testlp2_gmp,testlp3,testlp3_gmp,testshoot,testshoot_gmp}}) | (mkdir -p sage_wheels && cd sage_wheels && tar xvf -)'
     print(f'Running {command}')
     sys.stdout.flush()
-    if os.system(command) != 0: sys.exit(1)
+    if os.system(f"bash -c {shlex.quote(command)}") != 0: sys.exit(1)
