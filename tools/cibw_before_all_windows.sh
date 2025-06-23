eb38851defa6fed1393c5c1ff28943efed51f61a
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2025-06-22 22:22:59 -0700

    .github/workflows/dist-wheels-windows.yml: Windows continued

--- tools/cibw_before_all_windows.sh
+++ tools/cibw_before_all_windows.sh
@@ -4,4 +4,4 @@ pwd
 echo > constraints.txt
 PREFIX=$HOME/sage-local
 ./configure --enable-build-as-root --prefix=$PREFIX --with-sage-venv --with-system-gfortran=force --with-system-python3=force --without-system-libpng
-make V=0 $TARGETS_PRE
+MAKE="make -j6" make V=0 $TARGETS_PRE
