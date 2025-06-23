cf4414101ff6907c36fc0f44a3055b6e49fb21d9
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2025-06-23 09:09:06 -0700

    .github/workflows/dist-wheels-windows.yml: Use INCLUDE instead of CPATH

--- tools/cibw_before_all_windows.sh
+++ tools/cibw_before_all_windows.sh
@@ -2,6 +2,6 @@
 set -ex
 pwd
 echo > constraints.txt
-PREFIX=$HOME/sage-local
+PREFIX=$(pwd)/sage-local
 ./configure --enable-build-as-root --prefix=$PREFIX --with-sage-venv --with-system-gfortran=force --with-system-python3=force --without-system-libpng
 MAKE="make -j6" make V=0 $TARGETS_PRE
