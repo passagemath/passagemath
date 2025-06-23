82e1935b8fe48ebf3f257cf8bac91eb2931d43d4
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2025-06-23 15:24:41 -0700

    tools/cibw_before_all_windows.sh: If config.status exists, use it

--- tools/cibw_before_all_windows.sh
+++ tools/cibw_before_all_windows.sh
@@ -3,5 +3,10 @@ set -ex
 pwd
 echo > constraints.txt
 PREFIX=$(pwd)/sage-local
-./configure --enable-build-as-root --prefix=$PREFIX --with-sage-venv --with-system-gfortran=force --with-system-python3=force --without-system-libpng
+rm -rf sage-local/lib64  # avoid 'lib64 is not a symlink, see Issue #19782'
+if [ -x ./config.status ]; then
+    ./config.status
+else
+    ./configure --enable-build-as-root --prefix=$PREFIX --with-sage-venv --with-system-gfortran=force --with-system-python3=force --without-system-libpng
+fi
 MAKE="make -j6" make V=0 $TARGETS_PRE
