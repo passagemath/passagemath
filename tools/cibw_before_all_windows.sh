f2133d9e9a15e6e041a17342d5fcb7482f975a89
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2025-06-27 13:36:40 -0700

    tools/cibw_before_all_windows.sh: Use --enable-fat-binary

--- tools/cibw_before_all_windows.sh
+++ tools/cibw_before_all_windows.sh
@@ -7,6 +7,6 @@ rm -rf sage-local/lib64  # avoid 'lib64 is not a symlink, see Issue #19782'
 if [ -x ./config.status ]; then
     ./config.status
 else
-    ./configure --enable-build-as-root --prefix=$PREFIX --with-sage-venv --with-system-gfortran=force --with-system-python3=force --without-system-libpng
+    ./configure --enable-build-as-root --enable-fat-binary --prefix=$PREFIX --with-sage-venv --with-system-gfortran=force --with-system-python3=force --without-system-libpng
 fi
 MAKE="make -j6" make V=0 $TARGETS_PRE
