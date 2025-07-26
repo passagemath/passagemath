0200284e20ebd5fc0e8bd10ef02a379a9a30639b
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2025-07-26 12:53:05 -0700

    src/sage/all__sagemath_libecm.py: Fix merge

--- src/sage/all__sagemath_libecm.py
+++ src/sage/all__sagemath_libecm.py
@@ -1,7 +1,3 @@
 # sage_setup: distribution = sagemath-libecm
-<<<<<<< HEAD
-=======
-# delvewheel: patch
 
 from sage.all__sagemath_categories import *
->>>>>>> 579783d14f7 (pkgs/sagemath-libecm: Set up testing)
