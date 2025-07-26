6b31826296273335e989b9ab06212c6ac95a1bed
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2025-07-25 21:46:37 -0700

    src/sage/matrix/seymour_decomposition.p* (SymbolicNode): Remove

--- src/sage/matrix/seymour_decomposition.pyx
+++ src/sage/matrix/seymour_decomposition.pyx
@@ -4414,60 +4414,6 @@ cdef class PivotsNode(DecompositionNode):
         return self._child_nodes
 
 
-cdef class SymbolicNode(DecompositionNode):
-
-    def __init__(self, symbol, *, row_keys=None, column_keys=None, base_ring=None):
-        r"""
-        EXAMPLES::
-
-            sage: from sage.matrix.seymour_decomposition import SymbolicNode
-            sage: X = SymbolicNode('X', row_keys='abc', column_keys=range(6)); X
-            SymbolicNode X (3×6)
-            sage: XX = X.one_sum(X)
-            Traceback (most recent call last):
-            ...
-            ValueError: keys must be disjoint...
-            sage: XX = X.one_sum(X, summand_ids=(0, 1)); XX
-            OneSumNode (6×12) with 2 children
-            sage: XX.row_keys()
-            ((0, 'a'), (0, 'b'), (0, 'c'), (1, 'a'), (1, 'b'), (1, 'c'))
-            sage: T = XX.as_ordered_tree(); T
-            OneSumNode (6×12) with 2 children[SymbolicNode X (3×6)[],
-                                              SymbolicNode X (3×6)[]]
-            sage: unicode_art(T)
-            ╭────────────────OneSumNode (6×12) with 2 children
-            │                    │
-            SymbolicNode X (3×6) SymbolicNode X (3×6)
-            sage: Y = SymbolicNode('Y', row_keys='de', column_keys='fg'); Y
-            SymbolicNode Y (2×2)
-            sage: XY = X.one_sum(Y); XY
-            OneSumNode (5×8) with 2 children
-        """
-        super().__init__(row_keys=row_keys, column_keys=column_keys, base_ring=base_ring)
-        self._symbol = symbol
-
-    def _repr_(self):
-        r"""
-        Return a string representation of ``self``.
-
-        EXAMPLES::
-
-            sage: from sage.matrix.seymour_decomposition import SymbolicNode
-            sage: X = SymbolicNode('X', row_keys='abc', column_keys=range(6))
-            sage: print(X)
-            SymbolicNode X (3×6)
-        """
-        nrows, ncols = self.dimensions()
-        symbol = self.symbol()
-        return f'{self.__class__.__name__} {symbol} ({nrows}×{ncols})'
-
-    def matrix(self):
-        raise ValueError('symbolic nodes are not backed by CMR matrices')
-
-    def symbol(self):
-        return self._symbol
-
-
 cdef class ElementKey:
 
     cdef frozenset _key
