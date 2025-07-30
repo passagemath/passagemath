4d4f04feba978237a3b1060f021cfaf36592f8f0
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2025-07-30 09:37:14 -0700

    tools/cibw_repair_wheel_command_windows.sh: Use 'python'

--- tools/cibw_repair_wheel_command_windows.sh
+++ tools/cibw_repair_wheel_command_windows.sh
@@ -8,6 +8,6 @@ pkg=${pkg%%-*}
 pkg=${pkg//_/-}
 pkg=${pkg#pas}
 if [ -r pkgs/$pkg/repair_wheel.py ]; then
-    python3 pkgs/$pkg/repair_wheel.py ${wheel}
+    python pkgs/$pkg/repair_wheel.py ${wheel}
 fi
 delvewheel repair -vv --custom-patch -w ${dest_dir} ${wheel}
