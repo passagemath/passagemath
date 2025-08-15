2dba9ad3a45d0af4683f9ebe1d64304d65aad24d
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2025-08-14 23:01:05 -0700

    pkgs/sagemath-database-kohel: New

---
+++ pkgs/sagemath-database-kohel/repair_wheel.py
@@ -0,0 +1,31 @@
+# Add data to the wheel
+
+import os
+import shlex
+import sys
+
+from pathlib import Path
+
+from auditwheel.wheeltools import InWheel
+
+from sage_conf import SAGE_LOCAL
+
+if "TMPDIR" in os.environ:
+    os.environ["TMPDIR"] = str(Path(os.environ["TMPDIR"]).resolve())
+
+wheel = Path(sys.argv[1])
+
+with InWheel(wheel, wheel):
+    command = f'set -o pipefail; (cd {shlex.quote(SAGE_LOCAL)} && tar cf - --dereference share/kohel) | (mkdir -p sage_wheels && cd sage_wheels && tar xvf -)'
+    print(f'Running {command}')
+    sys.stdout.flush()
+    if os.system(f"bash -c {shlex.quote(command)}") != 0:
+        sys.exit(1)
+
+    # Remove the sage-conf dependency; it is not needed because our wheels ship what is needed.
+
+    command = 'sed -i.bak "/^Requires-Dist: passagemath-conf/d" *.dist-info/METADATA'
+    print(f'Running {command}')
+    sys.stdout.flush()
+    if os.system(f"bash -c {shlex.quote(command)}") != 0:
+        sys.exit(1)
--- src/sage/databases/db_class_polynomials.py
+++ src/sage/databases/db_class_polynomials.py
@@ -1,3 +1,4 @@
+# sage_setup: distribution = sagemath-database-kohel
 """
 Database of Hilbert polynomials
 
