397683b76ce37a71049ca86c382514e3bf72e544
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2025-11-03 17:06:25 -0800

    sage_setup.sage_setup: Do not use stable API on cp314t (fixup)

--- src/sage_setup/__init__.py
+++ src/sage_setup/__init__.py
@@ -119,7 +119,7 @@ def sage_setup(distributions, *,
         import sysconfig
 
         extension_kwds = {}
-        if py_limited_api and os.environ.get('CIBUILDWHEEL', None) and sys.version_info >= (3, 12, 0, 0) and sysconfig.get_config_var("Py_GIL_DISABLED") != '1':
+        if py_limited_api and os.environ.get('CIBUILDWHEEL', None) and sys.version_info >= (3, 12, 0, 0) and sysconfig.get_config_var("Py_GIL_DISABLED") != 1:
             # https://cibuildwheel.pypa.io/en/stable/options/#examples_8
             # https://cython.readthedocs.io/en/latest/src/userguide/limited_api.html#setuptools-and-setup-py
             extension_kwds['define_macros'] = [("Py_LIMITED_API", 0x030C0000)]
