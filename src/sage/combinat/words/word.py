c417a92a37b590f7a4d8dc6cf43dc9a50a7371eb
Chenxin Zhong <chenxin.zhong@outlook.com>
2025-12-05 16:35:34 +0800

    Improve doctests for pickling TypeError handling in InfiniteWord classes

--- src/sage/combinat/words/word.py
+++ src/sage/combinat/words/word.py
@@ -522,8 +522,9 @@ class InfiniteWord_iter_with_caching(WordDatatype_iter_with_caching, InfiniteWor
         sage: try:
         ....:     dumps(w)
         ....: except TypeError as e:
-        ....:     "pickle" in str(e) and "generator" in str(e)
-        True
+        ....:     if "pickle" in str(e) and "generator" in str(e)
+        ....:         print("TypeError raised in dumps() as expected")
+        TypeError raised in dumps() as expected
     """
     pass
 
@@ -562,8 +563,9 @@ class InfiniteWord_iter(WordDatatype_iter, InfiniteWord_class):
         sage: try:
         ....:     dumps(w)
         ....: except TypeError as e:
-        ....:     "pickle" in str(e) and "generator" in str(e)
-        True
+        ....:     if "pickle" in str(e) and "generator" in str(e)
+        ....:         print("TypeError raised in dumps() as expected")
+        TypeError raised in dumps() as expected
     """
     pass
 
@@ -665,8 +667,9 @@ class Word_iter_with_caching(WordDatatype_iter_with_caching, Word_class):
         sage: try:
         ....:     dumps(w)
         ....: except TypeError as e:
-        ....:     "pickle" in str(e) and "generator" in str(e)
-        True
+        ....:     if "pickle" in str(e) and "generator" in str(e):
+        ....:         print("TypeError raised in dumps() as expected")
+        TypeError raised in dumps() as expected
     """
     pass
 
@@ -703,8 +706,9 @@ class Word_iter(WordDatatype_iter, Word_class):
         sage: try:
         ....:     dumps(w)
         ....: except TypeError as e:
-        ....:     "pickle" in str(e) and "generator" in str(e)
-        True
+        ....:     if "pickle" in str(e) and "generator" in str(e):
+        ....:         print("TypeError raised in dumps() as expected")
+        TypeError raised in dumps() as expected
     """
     pass
 
