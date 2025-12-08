b4c62d5850feba5f526bd89f65ea4287c8700e21
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2025-12-07 19:17:50 -0800

    pkgs/sage-conf_pypi/sage_conf.py: Do not provide 'make' when SAGE_ROOT is undefined

--- pkgs/sage-conf_pypi/sage_conf.py
+++ pkgs/sage-conf_pypi/sage_conf.py
@@ -2,13 +2,18 @@ from _sage_conf._conf import *
 from _sage_conf.__main__ import _main
 
 
-def make(targets):
-    import os
-    SETENV = ':'
-    SETMAKE = 'if [ -z "$MAKE" ]; then export MAKE="make -j$(PATH=build/bin:$PATH build/bin/sage-build-num-threads | cut -d" " -f 2)"; fi'
-    cmd = f'cd {SAGE_ROOT} && ({SETENV}; {SETMAKE} && $MAKE V=0 {targets})'
-    if os.system(cmd) != 0:
-        raise RuntimeError(f"make {targets} failed")
+try:
+    SAGE_ROOT
+except NameError:
+    pass
+else:
+    def make(targets):
+        import os
+        SETENV = ':'
+        SETMAKE = 'if [ -z "$MAKE" ]; then export MAKE="make -j$(PATH=build/bin:$PATH build/bin/sage-build-num-threads | cut -d" " -f 2)"; fi'
+        cmd = f'cd {SAGE_ROOT} && ({SETENV}; {SETMAKE} && $MAKE V=0 {targets})'
+        if os.system(cmd) != 0:
+            raise RuntimeError(f"make {targets} failed")
 
 
 if __name__ == "__main__":
