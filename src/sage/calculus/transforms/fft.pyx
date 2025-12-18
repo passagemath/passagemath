8b825c191951a5ce92c77e878fbc9710d5356b7d
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2025-12-17 23:22:35 -0800

    src/sage/calculus/transforms: known bug: windows

--- src/sage/calculus/transforms/fft.pyx
+++ src/sage/calculus/transforms/fft.pyx
@@ -1,4 +1,5 @@
 # sage_setup: distribution = sagemath-modules
+# sage.doctest: needs numpy, known bug: windows (crash - https://github.com/passagemath/passagemath/issues/1892#issue-3726343166)
 """
 Fast Fourier Transforms Using GSL
 
