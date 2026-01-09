774588393339626b3020e14cd7b75dcfe8f225f3
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2026-01-08 20:07:02 -0800

    pkgs/sagemath-iml: New, split out from passagemath-linbox

---
+++ pkgs/sagemath-iml/passagemath_iml/__init__.py
@@ -0,0 +1,3 @@
+# sage_setup: distribution = sagemath-iml
+
+from sage.all__sagemath_iml import *
---
+++ pkgs/sagemath-iml/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+# PEP 517 builds do not have . in sys.path
+import os
+import sys
+sys.path.insert(0, os.path.dirname(__file__))
+
+from sage_setup import sage_setup
+
+sage_setup(['sagemath-iml'],
+           recurse_packages=('sage', 'passagemath_iml'),
+           required_modules=(),
+           spkgs=['iml'],
+           package_data={})
