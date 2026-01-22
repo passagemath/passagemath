7f881999c3d7266a5de40f6f239d1aaa803d04c6
Harshita Yadav <seemayadavanuj123@gmail.com>
2026-01-23 01:39:55 +0530

    Fix add_vertex bounds check and follow SageMath coding style

--- src/sage/graphs/base/c_graph.pyx
+++ src/sage/graphs/base/c_graph.pyx
@@ -1705,6 +1705,14 @@ cdef class CGraphBackend(GenericGraphBackend):
             - :meth:`has_vertex` -- returns whether or not this graph has a
               specific vertex
 
+        TESTS::
+
+            sage: G = Graph(320)
+            sage: G.add_vertex()
+            320
+            sage: G.add_vertex()
+            321
+
         EXAMPLES::
 
             sage: D = sage.graphs.base.dense_graph.DenseGraphBackend(9)
@@ -1726,16 +1734,9 @@ cdef class CGraphBackend(GenericGraphBackend):
         retval = None
         if name is None:
             name = 0
-            while (
-                name < self.cg().active_vertices.size and
-                (
-                    name in self.vertex_ints or
-                    (
-                        name not in self.vertex_labels and
-                        bitset_in(self.cg().active_vertices, <mp_bitcnt_t> name)
-                    )
-                )
-            ):
+            while name in self.vertex_ints or (
+                    name not in self.vertex_labels and
+                    name < self.cg().active_vertices.size and bitset_in(self.cg().active_vertices, <mp_bitcnt_t> name)):
                 name += 1
             retval = name
 
