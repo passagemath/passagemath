4d6305d432dc3426974bced130c7a31d14e2e92f
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2026-01-22 21:18:31 -0800

    src/sage/misc/{all__sagemath_modules.py,all.py}: Remove BackslashOperator

--- src/sage/misc/all.py
+++ src/sage/misc/all.py
@@ -3,13 +3,6 @@ from sage.misc.all__sagemath_environment import *
 from sage.misc.all__sagemath_modules import *
 from sage.misc.all__sagemath_repl import *
 
-from sage.misc.misc import (BackslashOperator,
-                            exists, forall, is_iterator,
-                            random_sublist,
-                            pad_zeros,
-                            newton_method_sizes, compose,
-                            nest)
-
 lazy_import('sage.misc.banner', 'banner', deprecation=34259)
 lazy_import('sage.misc.edit_module', 'set_edit_template', deprecation=34259)
 lazy_import('sage.misc.profiler', 'Profiler', deprecation=34259)
