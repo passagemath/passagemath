1121d07ea3d333a224afd91f14a2913b7c1f48f6
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2026-01-27 16:32:17 -0800

    Revert "src/sage/calculus/{interpolators,ode}.pyx: known bug: windows"
    This reverts commit d934e942628af8f2a5735d26c83f678cc452eeb9.

--- src/sage/calculus/ode.pyx
+++ src/sage/calculus/ode.pyx
@@ -1,5 +1,4 @@
 # sage_setup: distribution = sagemath-modules
-# sage.doctest: known bug: windows (crash - https://github.com/passagemath/passagemath/issues/1892#issue-3726343166)
 r"""
 Solving ODE numerically by GSL
 
