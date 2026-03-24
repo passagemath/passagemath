ee540784af0e9b6dee7545658239762954a56c36
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2026-03-24 14:49:36 -0700

    src/sage/libs/giac/__init__.py: No try...except for import

--- src/sage/libs/giac/__init__.py
+++ src/sage/libs/giac/__init__.py
@@ -1,7 +1,5 @@
 # sage_setup: distribution = sagemath-giac
 # sage.doctest: needs sage.libs.giac
-try:
-    from sage.libs.giac.gb import *
-    from sage.libs.giac.context import *
-except ImportError:
-    pass
+
+from sage.libs.giac.gb import *
+from sage.libs.giac.context import *
