ed5c90151f7991e92a17b425a31e880568cf27f3
Michael Orlitzky <michael@orlitzky.com>
2026-04-19 12:59:17 -0400

    src/sage/features/rankwidth.py: add copyright header

--- src/sage/features/rankwidth.py
+++ src/sage/features/rankwidth.py
@@ -2,6 +2,16 @@ r"""
 Feature for the rank-width graph decomposition
 """
 
+# ****************************************************************************
+#       Copyright (C) 2026 Michael Orlitzky <michael@orlitzky.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#                  https://www.gnu.org/licenses/
+# ****************************************************************************
+
 from sage.features import PythonModule
 from sage.features.join_feature import JoinFeature
 
