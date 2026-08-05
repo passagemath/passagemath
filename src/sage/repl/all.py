10983c9e724ce754d474c2d98c3fc9664ecf0e2a
Antonio Rojas <arojas@archlinux.org>
2026-08-05 09:20:59 +0200

    Import inline_plots on load
    So that inline plots can be configured without any further imports

--- src/sage/repl/all.py
+++ src/sage/repl/all.py
@@ -2,6 +2,7 @@
 from sage.misc.lazy_import import lazy_import
 
 from sage.repl.preparse import preparse, implicit_multiplication
+from sage.repl.interpreter import inline_plots
 
 lazy_import('sage.repl.interpreter', 'preparser')
 
