a14fe67828d00e4135b9c953ee9386bdf683295a
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2026-08-07 21:07:29 -0700

    src/sage/all__sagemath_gap3.py: Populate

--- src/sage/all__sagemath_gap3.py
+++ src/sage/all__sagemath_gap3.py
@@ -1,1 +1,15 @@
 # sage_setup: distribution = sagemath-gap3
+
+from sage.all__sagemath_gap import *
+
+
+try:
+    from sage.all__sagemath_combinat import *
+except ImportError:
+    pass
+
+
+try:
+    from sage.all__sagemath_modules import *
+except ImportError:
+    pass
