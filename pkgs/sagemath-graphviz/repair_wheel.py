c5d3843d04e3777270d95446eb0f42139df8e7d0
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2026-08-18 12:50:35 -0700

    pkgs/sagemath-graphviz: New

---
+++ pkgs/sagemath-graphviz/repair_wheel.py
@@ -0,0 +1,24 @@
+# Add data to the wheel
+
+import os
+import shlex
+import sys
+
+from pathlib import Path
+
+from auditwheel.wheeltools import InWheel
+
+from sage.config import SAGE_LOCAL
+
+if "TMPDIR" in os.environ:
+    os.environ["TMPDIR"] = str(Path(os.environ["TMPDIR"]).resolve())
+
+wheel = Path(sys.argv[1])
+
+# SAGE_LOCAL/bin/dot --> sage_wheels/bin/dot etc.
+with InWheel(wheel, wheel):
+    command = f'set -o pipefail; (cd {shlex.quote(SAGE_LOCAL)} && tar cf - --dereference bin/{dot,neato,twopi,fdp,circo}) | (mkdir -p sage_wheels && cd sage_wheels && tar xvf -)'
+    print(f'Running {command}')
+    sys.stdout.flush()
+    if os.system(f"bash -c {shlex.quote(command)}") != 0:
+        sys.exit(1)
