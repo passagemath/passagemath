9bd656704ac3bb90223316137d95446280b4c679
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2026-09-15 21:44:59 -0700

    ./sage -fixdoctests --verbose --long --fixed-point --only-tags --distribution sagemath-gap3 --distribution sagemath-standard-no-symbolics src/sage/combinat/root_system

--- src/sage/combinat/root_system/root_lattice_realizations.py
+++ src/sage/combinat/root_system/root_lattice_realizations.py
@@ -3097,7 +3097,7 @@ class RootLatticeRealizations(Category_over_base_ring):
                 sage: # needs sage.combinat sage.groups
                 sage: B = crystals.infinity.MVPolytopes(['C',2])                        # needs sage.graphs
                 sage: L = RootSystem(['C',2]).ambient_space()
-                sage: p = B.highest_weight_vector().f_string([1,2,1,2])
+                sage: p = B.highest_weight_vector().f_string([1,2,1,2])                 # needs sage.graphs
                 sage: L.plot_fundamental_weights() + L.plot_mv_polytope(p)              # needs sage.geometry.polyhedron sage.plot sage.symbolic
                 Graphics object consisting of 14 graphics primitives
 
@@ -3106,7 +3106,7 @@ class RootLatticeRealizations(Category_over_base_ring):
                 sage: # needs sage.combinat sage.groups
                 sage: B = crystals.infinity.MVPolytopes(['A',3])                        # needs sage.graphs
                 sage: L = RootSystem(['A',3]).ambient_space()
-                sage: p = B.highest_weight_vector().f_string([2,1,3,2])
+                sage: p = B.highest_weight_vector().f_string([2,1,3,2])                 # needs sage.graphs
                 sage: L.plot_mv_polytope(p)                                             # needs sage.geometry.polyhedron sage.plot sage.symbolic
                 Graphics3d Object
             """
