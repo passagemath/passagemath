c2e6f14d6a676aaeb27769de32b6a6df4faf76e4
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2026-09-18 15:27:05 -0700

    src/sage/calculus/tides.py: Remove sagelite

--- src/sage/calculus/tides.py
+++ src/sage/calculus/tides.py
@@ -1,3 +1,4 @@
+# sage_setup: distribution = sagemath-symbolics
 """
 Runtime helpers for the optional TIDES integrator.
 """
@@ -9,25 +10,7 @@ import shlex
 def _tides_compile_flags():
     """
     Return compiler flags for TIDES.
-
-    Normal Sage installations use ``SAGE_LOCAL``. Installed wheels can provide
-    the same redistributable headers and static library through the optional
-    ``sagelite-tides-runtime`` companion package.
     """
-    try:
-        from sagelite_tides.runtime import include_dir, library_path
-    except ImportError:
-        pass
-    else:
-        include = include_dir()
-        library = library_path()
-        if include.is_dir() and library.is_file():
-            return (
-                shlex.quote(os.fspath(library)),
-                "-L" + shlex.quote(os.fspath(library.parent)) + " ",
-                "-I" + shlex.quote(os.fspath(include)) + " ",
-            )
-
     sage_local = os.environ.get("SAGE_LOCAL", "$SAGE_LOCAL")
     return (
         os.path.join(sage_local, "lib", "libTIDES.a"),
