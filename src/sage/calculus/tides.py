33b37df9884973a2b6859802abc33ce133dcdf3f
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2026-09-18 15:53:43 -0700

    src/sage/calculus/tides.py: Use sage.env for SAGE_LOCAL

--- src/sage/calculus/tides.py
+++ src/sage/calculus/tides.py
@@ -6,12 +6,15 @@ Runtime helpers for the optional TIDES integrator.
 import os
 import shlex
 
+from sage.env import SAGE_LOCAL
+
 
 def _tides_compile_flags():
     """
     Return compiler flags for TIDES.
     """
-    sage_local = os.environ.get("SAGE_LOCAL", "$SAGE_LOCAL")
+    # FIXME: This should really go through sage.features.tides
+    sage_local = SAGE_LOCAL or "$SAGE_LOCAL"
     return (
         os.path.join(sage_local, "lib", "libTIDES.a"),
         os.path.join("-L" + sage_local, "lib "),
